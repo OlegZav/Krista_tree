a4fe810b09d3c3edcf5d2d52a13fd8afded25fff
Oleg Zavialov <oleg1111q@gmail.com>
2022-03-13 17:22:05 +0300

    Create new methods and fix tests

--- src/main/java/ru/yargu/prom/Node.java
+++ src/main/java/ru/yargu/prom/Node.java
@@ -1,30 +1,55 @@
 package ru.yargu.prom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
+    String name;
+    int id;
+    List<Node>children = new ArrayList<>();
+
     public Node(String name) {
+        this.name = name;
     }
 
     public String getName() {
-        return null;
+        return name;
+    }
+
+    public Boolean hasChild() {
+        return !children.isEmpty();
     }
 
     public Node createChild(String name) {
-        return null;
+        Node child = new Node(name);
+        children.add(child);
+        return child;
     }
 
-    public void addChild(Node name) {
+    public void addChild(Node child) {
+        children.add(child);
     }
 
     public Node findByName(String name) {
+        for (Node child : children) {
+            if (name.equals(child.getName()))
+                return child;
+        }
         return null;
     }
 
     public void removeChild(int id) {
+        for (Node child : children) {
+            if (child.id == id)
+                children.remove(id);
+        }
     }
 
-    public void removeAllChild(Node node) {
+    public void removeAllChild() {
+        children.clear();
     }
 
-    public void changeNode(Node node, String newValue) {
+    public void changeNode(String newName) {
+        this.name = newName;
     }
 }
--- src/test/java/ru/yargu/prom/NodeTest.java
+++ src/test/java/ru/yargu/prom/NodeTest.java
@@ -9,50 +9,52 @@ public class NodeTest {
     @Test
     void createTreeTest() {
         Node node = new Node("root");
+        assertNotNull(node, "Узел не создан");
         assertEquals("root", node.getName(), "Корневой узел создан с неправильным именем");
     }
 
     @Test
     void addChildTest() {
         Node node = new Node("root");
-        Node child1 = node.createChild("child1");
-        Node child2 = new Node("child2");
-        node.addChild(child2);
-        assertEquals("child2", node.findByName("child2").getName(), "");
+        Node child = node.createChild("child");
+        node.addChild(child);
+        assertTrue(node.hasChild(), "Созданный узел отсутствует");
     }
 
     @Test
     void searchChildTest() {
         Node node = new Node("root");
-        Node child1 = node.createChild("child1");
-        assertEquals("child1", node.findByName("child1").getName(), "");
+        Node child = node.createChild("child");
+        node.addChild(child);
+        assertEquals("child", node.findByName("child").getName(), "Искомый узел не найден");
     }
 
     @Test
     void deleteChildTest() {
         Node node = new Node("root");
-        Node child1 = node.createChild("child1");
-        Node child2 = node.createChild("child2");
-        node.removeChild(12);
-        assertNotEquals("child2", node.findByName("child2").getName(), "");
+        Node child = node.createChild("child");
+        node.addChild(child);
+        node.removeChild(child.id);
+        assertTrue(node.hasChild(), "Дочерний узел не удалён");
     }
 
     @Test
     void deleteAllChildTest() {
         Node node = new Node("root");
-        Node child1 = node.createChild("child1");
-        Node child2 = node.createChild("child2");
-        node.removeAllChild(node);
-        assertNotEquals("child1", node.findByName("child1").getName(), "");
-        assertNotEquals("child2", node.findByName("child2").getName(), "");
+        Node first_child = node.createChild("first_child");
+        node.addChild(first_child);
+        Node second_child = node.createChild("second_child");
+        node.addChild(second_child);
+        node.removeAllChild();
+        assertTrue(node.children.isEmpty(), "Остались дочерние узлы");
     }
 
     @Test
     void changeValueTest() {
         Node node = new Node("root");
-        Node child1 = node.createChild("child1");
-        node.changeNode(node, "newRoot");
-        assertEquals("newRoot", node.findByName("newRoot").getName(), "");
-//        assertNotEquals("root", node.findByName("root").getName(), "");
+        Node child = node.createChild("child");
+        node.addChild(child);
+        child.changeNode("newRoot");
+        assertEquals("newRoot", node.findByName("newRoot").getName(), "Узла с новым именем не обнаружено");
     }
 }
